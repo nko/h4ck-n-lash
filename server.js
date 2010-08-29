50acee90a5f7983ed27e7b3e3b5e5f897e991cd5
Micah Lee, micahflee & Ted Young, tedsuo <dev+micah+ted@radicaldesigns.org>
2010-08-29 15:53:53 -0700

    multiple levels

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var sys = require('sys');
 var ws = require('./vendor/node.ws.js/ws');
-//var player_pool = require('./lib/player_pool').create();
+var player_pool = require('./lib/player_pool').create();
 var http = require('http');
 var io = require('./vendor/socket.io');
+var levels = require('./lib/levels').levels();
+var player_pool = require('./lib/player_pool').create();
 
 static_port = process.env.NODE_PORT || 80;
 require('./lib/static_server').start(static_port);
@@ -14,16 +16,28 @@ var socket = io.listen( ws_server,{ transports:['websocket','flashsocket'] });
 
 socket.on( 'clientConnect', function(client){
   sys.log('new websocket connection: ' + client.sessionId);
-  client.send(JSON.stringify({event:'player_id',id:client.sessionId}));
+  
+  var level_id = Math.floor(Math.random()*levels.length);
+
+  player_pool.add_player_to_level( client, level_id);
+
+  client.send(JSON.stringify( {
+      event: 'player_connect', 
+      id: client.sessionId, 
+      level: levels[level_id], 
+      level_id: level_id
+  }));
 });
 
 socket.on('clientMessage', function( msg, client){
-//  sys.puts(sys.inspect(msg));
-  client.broadcast(msg);
+//  SYS.PUTS('MESSAGE: '+MSG);
+  message = JSON.parse(msg);
+  player_pool.broadcast_to_level( message.level_id, msg, client.sessionId);
 });
 
 socket.on('clientDisconnect', function(client){
   sys.puts('client diconnected: '+client.sessionId);
   client.broadcast(JSON.stringify({event:'die',id:client.sessionId})); 
+  
 });
 
