4f5c341f635493b231156e2319ff401411daafd6
Micah Lee <twopointfour@riseup.net>
2010-08-29 02:02:08 -0700

    in the middle of trying to auto-generate platforms based on selectors, but i have not finished and i am deciding to go to sleep soon instead

--- client/js/level.js
+++ client/js/level.js
@@ -4,33 +4,42 @@ var Level = function(level_id) {
   if(!level_id) level_id = 0;
   
   self.load_level = function(){
-  new_level = github;
-  self.id = new_level.id;
-  self.name = new_level.name; 
-  self.html_file = new_level.html_file;
-  self.width = new_level.width;
-  self.height = new_level.height;
-  self.platforms = new_level.platforms;
+    new_level = github;
+    self.id = new_level.id;
+    self.name = new_level.name; 
+    self.html_file = new_level.html_file;
+    self.width = new_level.width;
+    self.height = new_level.height;
+    self.selectors = new_level.selectors;
+    self.platforms = new_level.platforms || [];
+    $.ajax({
+      url: self.html_file,
+      success: function(data){
+        self.html = data;
 
-  $.ajax({
-    url: self.html_file,
-    success: function(data){
-      self.html = data;
-      level_loaded_event = jQuery.Event('level.loaded');
-      level_loaded_event.level = self;
-      $('body').trigger(level_loaded_event);
-    }
-  });
+        /*if(self.selectors) {
+          var new_platforms = [];
+          $.each(self.selectors, function(i,selector){
+            $(selector, self.html).each(function(i, dom){
+              //console.log(dom);
+              var offset = $(dom, self.html).offset();
+              var width  = $(dom, self.html).css('width');
+              new_platforms.push({y:offset.top,x:offset.left,x_end:offset.left+width});
+            });
+          });
+          self.platforms.concat(new_platforms);
+        }*/
 
-  // adjust the width of the game elements
-  $('#game-container').css('width', self.width).css('height', self.height);
-  $('#level-container').css('width', self.width).css('height', self.height);
-  $('#sprite-container').css('width', self.width).css('height', self.height);
+        level_loaded_event = jQuery.Event('level.loaded');
+        level_loaded_event.level = self;
+        $('body').trigger(level_loaded_event);
+      }
+    });
 
-  // load the appropriate stylesheet
-  //$('#styler').html("<link type='text/css' rel='stylesheet' href='"+this.stylesheet+"'/>");
-  
-  //return self;
+    // adjust the width of the game elements
+    $('#game-container').css('width', self.width).css('height', self.height);
+    $('#level-container').css('width', self.width).css('height', self.height);
+    $('#sprite-container').css('width', self.width).css('height', self.height);
   };
 
   self.load_level(level_id);
@@ -67,6 +76,7 @@ var github ={
   name: 'github',
   height:2500,
   width:1060,
+  selectors:['ul.org-members li', 'ul.repositories li.public', '.first', '.last'],
   platforms:[],
   html_file:'/levels/github.html'
 };
